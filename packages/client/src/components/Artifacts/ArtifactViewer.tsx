b6f7a9e97e0ab33311bef99457a2d4a94d6fa427
Cristobal Navarro <111240870+CristobalNavarroSchanentgen@users.noreply.github.com>
2025-06-22 20:13:21 +0200

    fix: correct regex patterns in ReactMarkdown function to fix syntax errors

--- packages/client/src/components/Artifacts/ArtifactViewer.tsx
+++ packages/client/src/components/Artifacts/ArtifactViewer.tsx
@@ -252,11 +252,11 @@ function ReactMarkdown({ children }: { children: string }) {
     .replace(/^### (.*$)/gim, '<h3>$1</h3>')
     .replace(/^## (.*$)/gim, '<h2>$1</h2>')
     .replace(/^# (.*$)/gim, '<h1>$1</h1>')
-    .replace(/^\\* (.*$)/gim, '<li>$1</li>')
-    .replace(/\\*\\*(.*?)\\*\\*/gim, '<strong>$1</strong>')
-    .replace(/\\*(.*?)\\*/gim, '<em>$1</em>')
-    .replace(/\\[([^\\]]+)\\]\\(([^\\)]+)\\)/gim, '<a href="$2">$1</a>')
-    .replace(/\\n/gim, '<br>');
+    .replace(/^\* (.*$)/gim, '<li>$1</li>')
+    .replace(/\*\*(.*?)\*\*/gim, '<strong>$1</strong>')
+    .replace(/\*(.*?)\*/gim, '<em>$1</em>')
+    .replace(/\[([^\]]+)\]\(([^)]+)\)/gim, '<a href="$2">$1</a>')
+    .replace(/\n/gim, '<br>');
 
   return <div dangerouslySetInnerHTML={{ __html: html }} />;
 }
